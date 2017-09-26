1cc82d3f125a57cea3f87fe2ee41e334abb7205e
xialg <xialiegang@gmail.com>
2017-09-26 08:58:26 +0800

    1

--- a.c
+++ a.c
@@ -1,8 +1,8 @@
 #include "stdio.h"
 int main()
 {
-    printf("hello world!");
-    int i=10;
-    printf("%d\n",i);
+    printf("hello world!\n");
+//    int i=10;
+//    printf("%d,%d,%d\n",i,i++,++i);
     getchar();
 }
